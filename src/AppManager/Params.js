99fb955fcbca385ae8334b53eb8c6e5714b534e2
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-03-18 23:05:52 +0200

    Update Params.js

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = 'e0b34aa4-10b8-4343-99d8-f0246628be51';
 export const appID = 'id6742878508';
 export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
-export const targetDate = '2025-03-19';
+export const targetDate = '2025-03-20';
 export const bodyLin = 'https://heroic-legendary-glory.space/2ZnrjM72';
